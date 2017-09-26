fdd2a6bc9182242dc65cb14315ccdba7fd492219
Kevin Foster <kevin.foster.uk@gmail.com>
2017-09-26 14:59:15 +0100

    Expose libs via main script

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = require('./lib/model-manager');;
+module.exports.ModelManager = require('./lib/model-manager');
+module.exports.ResourceLoader = require('./lib/resource-loader');
+module.exports.Repo = require('./lib/repo');
+module.exports.Service = require('./lib/service');
